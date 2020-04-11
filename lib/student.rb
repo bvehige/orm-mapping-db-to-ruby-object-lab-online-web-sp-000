e1228fe18a64a9dd6bb884ac7523a8fd5d6b408a
Brian Vehige <brianvehige@gmail.com>
2020-04-11 21:19:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,7 +86,7 @@ class Student
   def self.all_students_in_grade_X(grade)
     sql = <<-SQL
     SELECT * FROM students 
-    WHERE grade IS ? 
+    WHERE grade IS ?
     SQL
     
     DB[:conn].execute(sql, grade).map do |row|
